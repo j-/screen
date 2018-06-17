cccc56534d4c2a9421bb6e8dd9c4ffee9af1a615
j- <j@skeoh.com>
2018-06-17 17:29:00 +1000

    Add viz wrapper component

---
+++ src/VizWrapper.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import VizContainer from './VizContainer';
+import './VizWrapper.css';
+
+const VizWrapper: React.StatelessComponent = () => (
+	<div className="VizWrapper">
+		<VizContainer />
+	</div>
+);
+
+export default VizWrapper;
