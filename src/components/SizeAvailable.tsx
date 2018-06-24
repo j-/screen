b439b807d261cc1c2136a87008bbef1c8034db11
j- <j@skeoh.com>
2018-06-24 16:49:34 +1000

    Calculate difference in available pixels

--- src/components/SizeAvailable.tsx
+++ src/components/SizeAvailable.tsx
@@ -1,13 +1,34 @@
 import * as React from 'react';
 import { ImmutableScreen } from '../screen';
+import './SizeAvailable.css';
 
 export interface Props {
 	screen: ImmutableScreen;
 }
 
+function getTitle (left: number, right: number) {
+	if (left === right) {
+		// No difference. Do not assign a title.
+		return '';
+	}
+	return `Difference of ${Math.abs(left - right)} pixels`;
+}
+
 const SizeAvailable: React.StatelessComponent<Props> = ({ screen }) => (
 	<span className="SizeAvailable">
-		{screen.availWidth}x{screen.availHeight}
+		<span
+			className="SizeAvailable-width"
+			title={getTitle(screen.availWidth, screen.width)}
+		>
+			{screen.availWidth}
+		</span>
+		x
+		<span
+			className="SizeAvailable-height"
+			title={getTitle(screen.availHeight, screen.height)}
+		>
+			{screen.availHeight}
+		</span>
 	</span>
 );
 
